8a4a57b1a81e673392d2a33fc9600cf45a93abeb
Zygimantas Gatelis <zygis.gg@gmail.com>
2014-07-08 19:29:15 +0300

    Added plot 3

---
+++ plot3.R
@@ -0,0 +1,28 @@
+data = read.csv(
+  "data/household_power_consumption.txt",
+  header=TRUE, 
+  sep=";",
+  quote="", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+
+times <- strptime(paste(data[,1],data[,2]), "%d/%m/%Y %H:%M")
+
+par(mfrow = c(1,1))
+par(cex = 0.75)
+
+plot(
+  times,
+  data$Sub_metering_1, 
+  type="n", 
+  xlab="", 
+  ylab="Energi sub metering"
+)
+points(times,data$Sub_metering_1, type="l")
+points(times,data$Sub_metering_2, type="l", col="red")
+points(times,data$Sub_metering_3, type="l", col="blue")
+legend("topright", 
+       pch="-",
+       col = c("black", "red","blue"), 
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+)
+dev.copy(png, file = "plot3.png")
+dev.off() 
