1644a1ebbf27e31635c9e94b9bb92bf027c59023
Zygimantas Gatelis <zygimantas.gatelis@tieto.com>
2015-02-07 20:24:06 +0200

    Added plot4

--- plot4.R
+++ plot4.R
@@ -40,6 +40,9 @@ points(times,data$Sub_metering_2, type="l", col="red")
 points(times,data$Sub_metering_3, type="l", col="blue")
 legend("topright", 
        pch="-",
+       lty = 1,
+       cex = 0.75,
+       text.width = 59000,
        col = c("black", "red","blue"), 
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        bty="n"
